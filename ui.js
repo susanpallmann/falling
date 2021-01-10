92fb17ccf27ff129453da7e66fa87b56d009e358
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-01-10 01:38:03 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -2,18 +2,20 @@ $(document).on("mousemove", function(event) {
     $('#character').css('left', event.pageX - ($('#character').width()/2) + 'px');
 });
 
-var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
-$('#story').bind(mousewheelevt, function(e){
-    var evt = window.event || e //equalize event object     
-    evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
-    var delta = evt.detail ? evt.detail*(-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
+$(document).ready(function() {
+    var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+    $('#story').bind(mousewheelevt, function(e){
+        var evt = window.event || e //equalize event object     
+        evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
+        var delta = evt.detail ? evt.detail*(-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
 
-    if(delta > 0) {
-        //scroll up
-        console.log('up');
-    }
-    else{
-        //scroll down
-        console.log('down');
-    }   
+        if(delta > 0) {
+            //scroll up
+            console.log('up');
+        }
+        else{
+            //scroll down
+            console.log('down');
+        }   
+    });
 });
