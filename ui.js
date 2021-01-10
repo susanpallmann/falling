e57445fa4eb7638891032a135ed63b6f15a8b43f
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2021-01-10 02:01:57 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -3,19 +3,48 @@ $(document).on("mousemove", function(event) {
 });
 
 $(document).ready(function() {
+    
+    let phase = 0;
+    function updatePhase(val) {
+        if (phase !== 0 && phase !== 5) {
+            phase = phase + parseInt(val);
+        }
+        console.log(phase);
+    }
+    
+    var scrollTimer
+    var lastScrollFireTime = 0;
     var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
     $('#story').bind(mousewheelevt, function(e){
         var evt = window.event || e //equalize event object     
         evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
         var delta = evt.detail ? evt.detail*(-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
 
-        if(delta > 0) {
-            //scroll up
-            console.log('up');
+        var scrollInterval = 100;
+        var now = new Date().getTime();
+        
+        function processScroll() {
+            if(delta > 0) {
+                //scroll up
+                console.log('up');
+                updatePhase(-1);
+            }
+            else{
+                //scroll down
+                updatePhase(1);
+            } 
         }
-        else{
-            //scroll down
-            console.log('down');
-        }   
+        if (!scrollTimer) {
+            if (now - lastScrollFireTime > (3 * scrollInterval)) {
+                processScroll();   // fire immediately on first scroll
+                lastScrollFireTime = now;
+            }
+            scrollTimer = setTimeout(function() {
+                scrollTimer = null;
+                lastScrollFireTime = new Date().getTime();
+                processScroll();
+            }, scrollInterval);
+        }
+
     });
 });
